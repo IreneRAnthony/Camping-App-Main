4c8ace1629400c92e1bbdcdde42ab127db9d333f
Irene Anthony <56097221+IreneRAnthony@users.noreply.github.com>
2020-01-28 15:30:20 -0800

    Add files via upload

---
+++ server/controllers/trip.controller.js
@@ -0,0 +1,57 @@
+const mongoose = require('mongoose');
+require('../models/trip.model')
+const Trip = mongoose.model('Trip');
+
+module.exports = {
+    createTrip: function(req, res){
+        console.log('Creating trip...');
+        Trip.create(req.body)
+        .then(newTrip => {
+            console.log(newTrip);
+            res.json(newTrip);
+        })
+        .catch(err => res.json(err));
+    },
+
+    allTrips: function(req, res){
+        Trip.find()
+        .then(trip => {
+            res.json(trip);
+        })
+        .catch(err => res.json(err));
+    },
+
+    searchTrip: function(req, res){
+        Trip.findById({
+            _id: req.params.tripId
+        })
+        .then(trip => {
+            console.log('Got a trip!', trip);
+            res.json(trip);
+        })
+        .catch(err => res.json(err));
+    },
+
+    updateTrip: function(req, res){
+        Trip.findByIdAndUpdate({
+            _id: req.params.tripId
+        }, {
+            $set: req.body
+        })
+        .then(editedTrip => {
+            console.log('Edited!', editedTrip);
+            res.json(editedTrip);
+        })
+        .catch(err => res.json(err));
+    },
+
+    deleteTrip: function(req, res){
+        Trip.findByIdAndDelete({
+            _id: req.params.tripId
+        })
+        .then(deletedTrip => {
+            res.send();
+        })
+        .catch(err => res.json(err));
+    }
+}
