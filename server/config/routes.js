3502682c0e12a1e1ab5ddc6b3339badc1fd69560
Irene Anthony <56097221+IreneRAnthony@users.noreply.github.com>
2020-01-28 14:15:23 -0800

    Update routes.js

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,7 +1,15 @@
 const user = require("../controllers/user.controller")
 
 module.exports = function(app){
-    app.post('/new/user', (req, res) => {
+    app.get('/new/user', (req, res) => {
         user.createUser(req, res);
+    })
+
+    app.get('/login', (req, res) => {
+        user.login(req, res);
+    })
+
+    app.post('/logout', (req, res) => {
+        user.logout(req, res);
     })
 }
