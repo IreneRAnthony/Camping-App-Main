ca19cac8752af618ef7415013ef17375952ebe69
Irene Anthony <56097221+IreneRAnthony@users.noreply.github.com>
2020-01-28 14:33:25 -0800

    Add files via upload

---
+++ server/models/trip.model.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const TripSchema = new mongoose.Schema({
+    name: { type: String, require: true },
+    destination: { type: String, require: true },
+    start_date: {type: Date},
+    end_date: {type: Date},
+    going: {type: Array},
+    supply_list: {type: Array},
+    created_at: { type: Date, default: Date.now },
+    updated_at: { type: Date, default: Date.now }
+}, {timestamp: true})
+mongoose.model('Trip', TripSchema);
