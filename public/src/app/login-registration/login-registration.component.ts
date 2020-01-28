f4a405f77e8f8e039126c28f06bf24fc8cc89c42
Irene Anthony <56097221+IreneRAnthony@users.noreply.github.com>
2020-01-28 12:26:00 -0800

    Update login-registration.component.ts

--- public/src/app/login-registration/login-registration.component.ts
+++ public/src/app/login-registration/login-registration.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { HttpService } from '../http.service';
 
 @Component({
   selector: 'app-login-registration',
@@ -7,7 +9,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginRegistrationComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _httpService: HttpService, private _route: ActivatedRoute,
+    private _router: Router) { }
 
   ngOnInit() {
   }
