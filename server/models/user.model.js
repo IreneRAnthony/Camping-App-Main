bc308eb57f49820f983193d87018da2fcf2a5fa9
Irene Anthony <56097221+IreneRAnthony@users.noreply.github.com>
2020-01-28 11:14:47 -0800

    Add files via upload

---
+++ server/models/user.model.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    name: {type: String, required: true},
+    password: {type: String, required: true},
+    email: {type: String, required: true},
+    created_at: { type: Date, default: Date.now },
+    updated_at: { type: Date, default: Date.now }
+}, {timestamp: true})
+mongoose.model('User', UserSchema);
