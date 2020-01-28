d817559894c8131711c7792c267dcfc81b701d31
Irene Anthony <56097221+IreneRAnthony@users.noreply.github.com>
2020-01-28 12:48:35 -0800

    Update user.controller.js

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -3,5 +3,13 @@ require('../models/user.model')
 const User = mongoose.model('User');
 
 module.exports = {
-    
+    createUser: function(req, res){
+        console.log("Creating user...");
+        User.create(req.body)
+        .then(newUser => {
+            console.log(newUser);
+            res.json(newUser);
+        })
+        .catch(err => res.json(err));
+    }
 }
