be300a8c7f0f01eef7e902179508e1eea8f36b16
Irene Anthony <56097221+IreneRAnthony@users.noreply.github.com>
2020-01-28 12:05:15 -0800

    Update app-routing.module.ts

--- public/src/app/app-routing.module.ts
+++ public/src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginRegistrationComponent } from './login-registration/login-registration.component'
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '/login', component: LoginRegistrationComponent },
+  { path: '', pathMatch: 'full', redirectTo: '/login' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
