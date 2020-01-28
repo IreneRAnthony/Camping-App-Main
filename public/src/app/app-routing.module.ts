36396d2ca7d91fd5210d05840545b09171a84061
Irene Anthony <56097221+IreneRAnthony@users.noreply.github.com>
2020-01-28 15:01:55 -0800

    Update app-routing.module.ts

--- public/src/app/app-routing.module.ts
+++ public/src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginRegistrationComponent } from './login-registration/login-registration.component'
+import { NewLocationComponent } from './new-location/new-location.component';
+import { AboutLocationComponent } from './about-location/about-location.component';
+import { NewTripComponent } from './new-trip/new-trip.component';
 
 const routes: Routes = [
+  { path: '/new/location', component: NewLocationComponent },
+  { path: '/new/trip', component: NewTripComponent },
+  { path: '/about/location/:id', component: AboutLocationComponent },
   { path: '/login', component: LoginRegistrationComponent },
   { path: '', pathMatch: 'full', redirectTo: '/login' }
 ];
