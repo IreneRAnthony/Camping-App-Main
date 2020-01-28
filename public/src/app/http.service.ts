192694750227e9db044a70f70bc1bfe92444dbd1
Irene Anthony <56097221+IreneRAnthony@users.noreply.github.com>
2020-01-28 12:32:45 -0800

    Update http.service.ts

--- public/src/app/http.service.ts
+++ public/src/app/http.service.ts
@@ -6,5 +6,10 @@ import { HttpClient } from '@angular/common/http';
 })
 export class HttpService {
 
+  registerUser(newUser){
+    return this._http.post('/new/user', newUser);
+  }
+
   constructor(private _http: HttpClient) { }
 }
+
