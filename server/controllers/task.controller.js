2ed4ff0965ab304b91e813d342b212b5416c97dc
Irene Anthony <56097221+IreneRAnthony@users.noreply.github.com>
2020-01-29 09:52:49 -0800

    Add files via upload

---
+++ server/controllers/task.controller.js
@@ -0,0 +1,62 @@
+const mongoose = require('mongoose');
+require('../models/task.model')
+const Task = mongoose.model('Task');
+
+module.exports = {
+    createTask: function(req, res){
+        console.log('Creating task...');
+        Task.create({
+            title: req.body.title,
+            description: req.body.description,
+            priority: req.body.priority,
+            completed: req.body.priority,
+            creator: user._id
+        })
+        .then(newTask => {
+            res.json(newTask);
+            console.log('Created Task!', newTask);
+        })
+        .catch(err => res.json(err));
+    },
+
+    allTasks: function(req, res){
+        console.log("Retrieving all tasks...");
+        Task.find()
+        .then(allTasks => {
+            res.json(allTasks);
+        })
+        .catch(err => res.json(err));
+    },
+
+    searchTasks: function(req, res){
+        console.log("Searching for task...");
+        Task.findById({
+            _id: req.params.taskId
+        })
+        .then(foundTask => {
+            res.json(foundTask);
+        })
+        .catch(err => res.json(err));
+    },
+
+    updateTask: function(req, res){
+        Task.findByIdAndUpdate({
+            _id: req.params.taskId
+        })
+        .then(updatedTask => {
+            res.json(updatedTask);
+        })
+        .catch(err => res.json(err));
+    },
+
+    deleteTask: function(req, res){
+        Task.findByIdAndDelete({
+            _id: req.params.taskId
+        })
+        .then(deletedTask => {
+            res.send()
+        })
+        .catch(err => res.json(err));
+    }
+
+}
