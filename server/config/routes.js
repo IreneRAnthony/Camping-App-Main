06598c657dc5bbf12cf66384fac8304359d98ba8
Irene Anthony <56097221+IreneRAnthony@users.noreply.github.com>
2020-01-29 09:53:50 -0800

    Update routes.js

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,4 +1,7 @@
 const user = require("../controllers/user.controller");
+const trip = require('../controllers/trip.controller');
+const task = require('../controllers/task.controller');
+const location = require('../controllers/location.controller');
 
 // added the server argument to the function for the sockets to work 
 module.exports = function(app, server){
