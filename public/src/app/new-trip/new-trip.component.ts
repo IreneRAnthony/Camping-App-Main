e9f43f49f3a97ba1cbb109bd0f465faa42def213
Irene Anthony <56097221+IreneRAnthony@users.noreply.github.com>
2020-01-28 16:09:29 -0800

    Add files via upload

---
+++ public/src/app/new-trip/new-trip.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { HttpService } from '../http.service';
+import { Trip } from '../trip';
+
+@Component({
+  selector: 'app-new-trip',
+  templateUrl: './new-trip.component.html',
+  styleUrls: ['./new-trip.component.css']
+})
+export class NewTripComponent implements OnInit {
+  newTrip: Trip;
+  errors;
+
+  constructor(private _httpService: HttpService, private _route: ActivatedRoute,
+    private _router: Router) { }
+
+  ngOnInit() {
+    this.newTrip = new Trip();
+  }
+
+  clickCreateTrip(){
+    this._httpService.createTrip(this.newTrip).subscribe( (newTrip: any) => {
+      if(newTrip.errors){
+        this.errors = newTrip.errors;
+      } else {
+        console.log('Created trip', newTrip);
+        this.newTrip = new Trip();
+        this._router.navigate(['/home']);
+      }
+    })
+  }
+
+}
