e35ea0eccb647d5bec789d54e4aac9f04d8958a6
Irene Anthony <56097221+IreneRAnthony@users.noreply.github.com>
2020-01-30 09:02:07 -0800

    Update user.model.js

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -4,6 +4,7 @@ const UserSchema = new mongoose.Schema({
     name: {type: String, required: true},
     password: {type: String, required: true},
     email: {type: String, required: true},
+    wishlist: {type: Array, required: false},
     created_at: { type: Date, default: Date.now },
     updated_at: { type: Date, default: Date.now },
     // messages db 
