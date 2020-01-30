e5da2c6eacaccbebd45e921eb6e1711ffdb87f1a
Irene Anthony <56097221+IreneRAnthony@users.noreply.github.com>
2020-01-30 13:11:11 -0800

    Update supply.controller.js

--- server/controllers/supply.controller.js
+++ server/controllers/supply.controller.js
@@ -5,7 +5,11 @@ const Supply = mongoose.model('Supply');
 module.exports = {
     createSupply: function(req, res){
         console.log('Creating supply...')
-        Supply.create(req.body)
+        Supply.create({
+            item: req.body.item,
+            status: false,
+            trip_id: req.params.id
+        })
         .then(newSupply => {
             res.json(newSupply)
         })
@@ -25,7 +29,12 @@ module.exports = {
 
     updateSupply: function(req, res){
         console.log('Updating supply...')
-        Supply.findByIdAndUpdate(req.params.id, req.body)
+        Supply.findByIdAndUpdate(req.params.id, {
+            $set: {
+                item: req.body.title,
+                status: req.body.status
+            }
+        })
         .then(updatedSupply => {
             res.json(updatedSupply);
         })
@@ -34,14 +43,14 @@ module.exports = {
 
     deleteSupply: function(req, res){
         console.log('Deleting supply...')
-        Supply.findByIdAndDelete(req.params.id)
+        Supply.findByIdAndDelete(req.params.id, req.body)
         .then(deletedSupply => {
             console.log(deletedSupply)
             res.send();
         })
         .catch(err => res.json(err))
     },
-    
+
     completeSupply: function(req, res){
         console.log('Supply obtained!')
         Supply.findByIdAndUpdate(req.params.id, {
