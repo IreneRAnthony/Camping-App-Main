3bbb20a26a9acdaadb0e5e92f0c40cb81b6d17e3
Irene Anthony <56097221+IreneRAnthony@users.noreply.github.com>
2020-01-31 13:28:26 -0800

    Update new-trip.component.ts

--- public/src/app/new-trip/new-trip.component.ts
+++ public/src/app/new-trip/new-trip.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { HttpService } from '../http.service';
-import { Trip } from '../trip';
+import { Trip } from '../models/trip';
 
 @Component({
   selector: 'app-new-trip',
