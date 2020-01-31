9a78abe438436bf1be60f30b017a95a29cbbfdde
Irene Anthony <56097221+IreneRAnthony@users.noreply.github.com>
2020-01-31 10:35:16 -0800

    Update login-registration.component.ts

--- public/src/app/login-registration/login-registration.component.ts
+++ public/src/app/login-registration/login-registration.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { HttpService } from '../http.service';
+import { User } from '../models/user';
 
 @Component({
   selector: 'app-login-registration',
@@ -8,11 +9,46 @@ import { HttpService } from '../http.service';
   styleUrls: ['./login-registration.component.css']
 })
 export class LoginRegistrationComponent implements OnInit {
+  newUser: User;
+  returnUser: User;
+  errors;
 
-  constructor(private _httpService: HttpService, private _route: ActivatedRoute,
-    private _router: Router) { }
+  constructor(
+    private _httpService: HttpService,
+    private _route: ActivatedRoute,
+    private _router: Router) {}
 
   ngOnInit() {
+    this.newUser = new User();
+    this.returnUser = new User();
+    // this.findAllUsers();
+    this._httpService.logoutUser();
+  }
+
+  findAllUsers(){
+    this._httpService.findAllUsers().subscribe((allUsers) => {
+      console.log(allUsers);
+    })
+  }
+
+  clickRegister() {
+    this._httpService.registerUser(this.newUser).subscribe((registeredUser: any) => {
+      console.log('User registered', registeredUser)
+      this._router.navigate(['/login'])
+    },
+    error => {
+      this.errors = error;
+    })
+  }
+
+  clickLogin() {
+    this._httpService.loginUser(this.returnUser).subscribe(loggedUser => {
+      console.log('Logged in', loggedUser);
+      this._router.navigate(['/home']);
+    },
+    error => {
+      this.errors = error;
+    })
   }
 
 }
