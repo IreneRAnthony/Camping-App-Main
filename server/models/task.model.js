8259ad8ea8d5b0b70e369cd0a6fba120775d3bbd
Irene Anthony <56097221+IreneRAnthony@users.noreply.github.com>
2020-01-31 13:41:08 -0800

    Update task.model.js

--- server/models/task.model.js
+++ server/models/task.model.js
@@ -4,7 +4,6 @@ const TaskSchema = new mongoose.Schema({
     title: {type: String, required: true},
     description: {type: String, default: ""},
     priority: {type: String, default: 'Low'},
-    completed: {type: Boolean, default: false},
     creator: {type: String}
 }, {timestamps: true})
 mongoose.model('Task', TaskSchema);
